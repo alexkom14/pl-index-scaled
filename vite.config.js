78790bf2cd0ca448605f392290cf28982657df7d
Alex <alexkomn@csd.auth,gr>
2026-07-27 16:05:13 +0300

    changed base vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pl-index-scaled/',
+  base: '/pl-Index-scaled/',
   plugins: [react()],
   server: {
     proxy: {
